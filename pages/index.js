d635f7a250aa16b234df8f8626624ceb6a21a43c
Sohaib Mubashir <sohaibmh@yahoo.com>
2022-04-16 01:42:51 +0100

    added schema data

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,13 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Appliance Repair Service in London | Appliance Vitals Ltd</title>
+        <script
+          type="application/ld+json"
+          data-script-src="true"
+          dangerouslySetInnerHTML={{
+            __html: ` { "@context":"https://schema.org", "@type":"LocalBusiness", "name": "Appliance Vitals London", "url": "https://www.appliancevitals.com/", "image": "https://www.appliancevitals.com/_next/image?url=%2Fmiele-washing-machine.webp&w=1920&q=100", "logo": "https://www.appliancevitals.com/_next/image?url=%2Flogo-transparent-168-min.png&w=256&q=100", "priceRange": "££", "paymentAccepted": "Credit Card, Debit Card, Invoice", "currenciesAccepted":"gbp", "openingHours":"Mo-Su 7am-7pm", "telephone":"+7429184409", "address": { "@type": "PostalAddress", "addressLocality": "Central London", "addressRegion": "London", "postalCode": "EN4 8PH" }, "contactPoint": [{ "@type" : "ContactPoint", "telephone": "+7429184409", "contactType": "Customer Service" }], "aggregateRating": { "@type":"AggregateRating", "ratingValue": 5.0, "ratingCount": 18 } } `,
+          }}
+        ></script>
         <meta charSet="UTF-8"></meta>
         <meta
           name="description"
